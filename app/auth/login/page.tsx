66bdd38d914369da84576a1f7141f93f4dabdebf
Waleed Ahmad <waleedcoderz@gmail.com>
2026-02-19 16:30:54 +0500

    structure change

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -17,7 +17,7 @@ export default function LoginPage() {
     e.preventDefault();
     setLoading(true);
     try {
-      const response = await axios.post(`https://respectable-fionnula-personaluseprojects-818b9efd.koyeb.app/api/login`, {
+      const response = await axios.post(`https://backendrepoo-production.up.railway.app/api/login`, {
         email: email.trim(), // Trim spaces
         password
       });
