1e368dacdd823f3f87ce3645a4e6fd51d3717701
waleedcoder786 <waleedcoderz@gmail.com>
2026-03-05 01:34:37 +0500

    Update page.tsx

--- app/editpaper/[id]/page.tsx
+++ app/editpaper/[id]/page.tsx
@@ -18,8 +18,8 @@ import Link from "next/link";
 import axios from "axios";
 import { PaperHeader } from "../../components/headers"; 
 
-// const API_BASE = "https://backendrepoo-production.up.railway.app/api";
-const API_BASE = "http://localhost:5000/api"; 
+const API_BASE = "https://backendrepoo-production.up.railway.app/api";
+// const API_BASE = "http://localhost:5000/api"; 
 
 // --- HELPER COMPONENT: Auto-Resizing Textarea ---
 const AutoResizeTextarea = ({ value, onChange, className, style, placeholder }: any) => {
